27626c95ae725392699ca9c325a9f867039c85e5
Gunda manideep <139052776+manideepgunda27@users.noreply.github.com>
2024-11-26 15:48:35 +0530

    Create roleRoutes.js

---
+++ backend/routes/roleRoutes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const { getRoles, createRole } = require("../controllers/roleController");
+
+router.get("/", getRoles);
+router.post("/", createRole);
+
+module.exports = router;
